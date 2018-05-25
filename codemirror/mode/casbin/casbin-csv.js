d40c5fc371cae1bdf45f9bd00d144b3bf5fc5596
Yang Luo <hsluoyz@qq.com>
2018-05-25 22:51:32 +0800

    Add casbin-csv mode.

---
+++ codemirror/mode/casbin/casbin-csv.js
@@ -0,0 +1,28 @@
+// CodeMirror, copyright (c) by Casbin
+// Distributed under an MIT license: http://codemirror.net/LICENSE
+
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+  "use strict";
+
+  CodeMirror.defineMode("casbin-csv", function() {
+    function tokenBase (stream, state) {
+      stream.next();
+    }
+
+    return {
+      startState: function () {
+        return {tokenize: tokenBase};
+      },
+      token: function (stream, state) {
+        return state.tokenize(stream, state);
+      }
+    };
+  });
+});
