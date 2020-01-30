3de2e675aad9f5d51f01f1a2888323a3fefd275b
Yang Luo <hsluoyz@qq.com>
2020-01-30 13:28:40 +0800

    Updates

---
+++ precache-manifest.e1db5d722e542a792947dfa04b99c97b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c83bed74efec8bad206e8582673a4d",
+    "url": "/casbin-editor/index.html"
+  },
+  {
+    "revision": "8122f89eeddf01b2772b",
+    "url": "/casbin-editor/static/css/2.ced4bf2c.chunk.css"
+  },
+  {
+    "revision": "8122f89eeddf01b2772b",
+    "url": "/casbin-editor/static/js/2.d35cbe3a.chunk.js"
+  },
+  {
+    "revision": "f1549f38c9b06877c0d0",
+    "url": "/casbin-editor/static/js/main.75bc2d93.chunk.js"
+  },
+  {
+    "revision": "65c7fa19bea0a4983ffd",
+    "url": "/casbin-editor/static/js/runtime-main.88e14df3.js"
+  }
+]);
