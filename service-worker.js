141c687a5a9cc38fb318302aa7270acacc28cbc4
hsluoyz <hsluoyz@qq.com>
2020-11-03 09:43:19 +0000

    Deploying to gh-pages from  @ 09bb66e9fe9ce2e604f66f797849c0652e17806c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/casbin-editor/precache-manifest.2b15d19c7d6b84e92870813cefe3ac17.js"
+  "/casbin-editor/precache-manifest.c2beba9867fe0b08b9fcf1f956db7858.js"
 );
 
 self.addEventListener('message', (event) => {
