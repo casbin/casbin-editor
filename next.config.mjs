d06b2f0fcd15c9f5181c766cf96e3a0cf4993876
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-02-24 11:57:10 +0800

    fix: fix deploy basePath

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "",
+  basePath: "/casbin-editor",
 
   /**
    * Disable server-based image optimization. Next.js does not support
