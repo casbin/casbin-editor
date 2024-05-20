a99fab3fbc60b6837bde335d72eb82e2a5f0a10b
hashcookie <hashcookie404@gmail.com>
2024-05-20 12:54:30 +0800

    style(editor): remove unnecessary line

--- app/components/editor/index.tsx
+++ app/components/editor/index.tsx
@@ -197,7 +197,6 @@ export const EditorScreen = () => {
                     );
                   })}
                 </select>
-
                 <button
                   className={clsx(
                     'rounded',
