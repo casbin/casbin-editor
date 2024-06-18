a2b5c9afcecb2620eb66b511860b4a98e978f03c
hashcookie <hashcookie404@gmail.com>
2024-06-18 14:30:45 +0800

    refactor: replace existing sidebar with Ant Design Drawer component

--- app/components/SidePanelChat.tsx
+++ app/components/SidePanelChat.tsx
@@ -1,30 +1,43 @@
-'use client';
-
-import { Drawer } from 'vaul';
+import React, { useState } from 'react';
+import { Drawer } from 'antd';
 import ChatWindow from './ChatWindow';
 
-export function SidePanelChat() {
+export const SidePanelChat: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Drawer.Root direction="right">
-      <Drawer.Trigger asChild>
-        <button className="text-red-600">Why this result?</button>
-      </Drawer.Trigger>
-      <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0" />
-        <Drawer.Content className="bg-white flex flex-col rounded-t-[10px] h-full w-[500px] fixed bottom-0 right-0">
-          <div className="p-4 bg-white flex-1 flex flex-col h-full">
-            <Drawer.Title className="font-medium mb-4">
-              <a target="_blank" className="inline-flex items-center" rel="noreferrer" href={'https://casdoor.com'}>
-                <img alt="help" src="https://casbin.org/img/casbin.svg" className="h-5 w-5 mr-2 mb-1" />
-                <div>AI Assistant</div>
-              </a>
-            </Drawer.Title>
-            <div className="flex-1 flex flex-col">
-              <ChatWindow />
-            </div>
-          </div>
-        </Drawer.Content>
-      </Drawer.Portal>
-    </Drawer.Root>
+    <>
+      <button className="text-red-600" onClick={showDrawer}>
+        Why this result?
+      </button>
+      <Drawer
+        title={
+          <a target="_blank" className="inline-flex items-center" rel="noreferrer" href={'https://casdoor.com'}>
+            <img alt="help" src="https://casbin.org/img/casbin.svg" className="h-5 w-5 mr-2 mb-1" />
+            <div>AI Assistant</div>
+          </a>
+        }
+        placement="right"
+        onClose={onClose}
+        open={open}
+        width={500}
+        styles={{
+          body: { display: 'flex', flexDirection: 'column', height: '100%' },
+          mask: { backgroundColor: 'transparent' },
+        }}
+      >
+        <div className="flex-1 flex flex-col">
+          <ChatWindow />
+        </div>
+      </Drawer>
+    </>
   );
-}
+};
