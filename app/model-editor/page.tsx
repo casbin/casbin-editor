1bb5d3874332d191b444000ea235209a9d92e1de
Coki <hashcookie404@gmail.com>
2024-09-15 12:01:36 +0800

    refactor: simplify ModelEditor component and improve message handling

--- app/model-editor/page.tsx
+++ app/model-editor/page.tsx
@@ -1,22 +1,12 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ModelEditor } from '../components/ModelEditor';
 
 export default function ModelEditorPage() {
-  const [initialValue, setInitialValue] = useState('');
-
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const modelParam = params.get('model');
-    if (modelParam) {
-      setInitialValue(decodeURIComponent(modelParam));
-    }
-  }, []);
-
   return (
     <div style={{ width: '100%', height: '100vh' }}>
-      <ModelEditor initialValue={initialValue} />
+      <ModelEditor />
     </div>
   );
 }
