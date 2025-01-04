b62669a9c280fdcba1b0badb7bb231bbfb8f7e92
Coki <hashcookie404@gmail.com>
2025-01-04 14:51:58 +0800

    refactor: remove unused useCopy hook and related share state in EditorScreen

--- app/components/editor/index.tsx
+++ app/components/editor/index.tsx
@@ -13,7 +13,6 @@ import { CasbinPolicySupport } from '@/app/components/editor/casbin-mode/casbin-
 import { javascriptLanguage } from '@codemirror/lang-javascript';
 import useRunTest from '@/app/components/editor/hooks/useRunTest';
 import useShareInfo from '@/app/components/editor/hooks/useShareInfo';
-import useCopy from '@/app/components/editor/hooks/useCopy';
 import useSetupEnforceContext from '@/app/components/editor/hooks/useSetupEnforceContext';
 import useIndex from '@/app/components/editor/hooks/useIndex';
 import SidePanelChat from '@/app/components/SidePanelChat';
@@ -38,8 +37,6 @@ export const EditorScreen = () => {
     requestResult,
     setRequestResult,
     customConfig,
-    share,
-    setShare,
     enforceContextData,
     setPolicyPersistent,
     setModelTextPersistent,
@@ -51,7 +48,6 @@ export const EditorScreen = () => {
   const [open, setOpen] = useState(true);
   const { enforcer } = useRunTest();
   const { shareInfo } = useShareInfo();
-  const { copy } = useCopy();
   const { setupEnforceContextData, setupHandleEnforceContextChange } = useSetupEnforceContext({
     onChange: setEnforceContextDataPersistent,
     data: enforceContextData,
@@ -74,7 +70,7 @@ export const EditorScreen = () => {
   const engineGithubLinks = {
     node: `https://github.com/casbin/node-casbin/releases/tag/v${casbinVersion}`,
     java: 'https://github.com/casbin/jcasbin/releases',
-    go: 'https://github.com/casbin/casbin/releases'
+    go: 'https://github.com/casbin/casbin/releases',
   };
 
   useEffect(() => {
@@ -275,21 +271,18 @@ export const EditorScreen = () => {
                 <select
                   className="bg-transparent border border-[#e13c3c] rounded px-2 py-1 text-[#e13c3c] focus:outline-none"
                   value={selectedEngine}
-                  onChange={(e) => {return setSelectedEngine(e.target.value)}}
+                  onChange={(e) => {
+                    return setSelectedEngine(e.target.value);
+                  }}
                 >
                   <option value="node">Node-Casbin(NodeJs) v{casbinVersion}</option>
                   <option value="java">jCasbin(Java)</option>
                   <option value="go">Casbin(Go)</option>
                 </select>
-                <a
-                  href={engineGithubLinks[selectedEngine]}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-[#e13c3c] hover:text-[#ff4d4d]"
-                >
+                <a href={engineGithubLinks[selectedEngine]} target="_blank" rel="noopener noreferrer" className="text-[#e13c3c] hover:text-[#ff4d4d]">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
                     {/* eslint-disable-next-line max-len */}
-                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                   </svg>
                 </a>
               </div>
@@ -453,7 +446,9 @@ export const EditorScreen = () => {
               )}
               onClick={() => {
                 shareInfo({
-                  onResponse: (v) => {return handleShare(v)},
+                  onResponse: (v) => {
+                    return handleShare(v);
+                  },
                   modelKind,
                   model: modelText,
                   policy,
