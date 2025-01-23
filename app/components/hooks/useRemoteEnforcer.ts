f789586e3d47a4621ed41a1418501dad553b839a
Coki <hashcookie404@gmail.com>
2025-01-23 15:08:51 +0800

    refactor: extract endpoint retrieval logic into `getEndpoint` function

--- app/components/hooks/useRemoteEnforcer.ts
+++ app/components/hooks/useRemoteEnforcer.ts
@@ -26,9 +26,17 @@ export interface VersionInfo {
 
 export const DEFAULT_ENDPOINT = 'door.casdoor.com';
 
+export const getEndpoint = () => {
+  try {
+    return window?.localStorage?.getItem('casbinEndpoint') || DEFAULT_ENDPOINT;
+  } catch {
+    return DEFAULT_ENDPOINT;
+  }
+};
+
 export async function remoteEnforcer(props: RemoteEnforcerProps) {
   try {
-    const baseUrl = `https://${window.localStorage.getItem('casbinEndpoint') || DEFAULT_ENDPOINT}/api/run-casbin-command`;
+    const baseUrl = `https://${getEndpoint()}/api/run-casbin-command`;
     const args = [
       'enforceEx',
       '-m',
@@ -86,7 +94,7 @@ export async function remoteEnforcer(props: RemoteEnforcerProps) {
 
 export async function getRemoteVersion(language: 'java' | 'go'): Promise<VersionInfo> {
   try {
-    const baseUrl = `https://${window.localStorage.getItem('casbinEndpoint') || DEFAULT_ENDPOINT}/api/run-casbin-command`;
+    const baseUrl = `https://${getEndpoint()}/api/run-casbin-command`;
     const url = new URL(baseUrl);
     url.searchParams.set('language', language);
     url.searchParams.set('args', JSON.stringify(['-v']));
