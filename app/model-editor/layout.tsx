a05a721e039cf2b28ac401a8e41e12d8b5acd017
Coki <hashcookie404@gmail.com>
2025-02-06 22:57:57 +0800

    feat: improve code formatting and cleanup

--- app/model-editor/layout.tsx
+++ app/model-editor/layout.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-export default function ModelEditorLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function ModelEditorLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
